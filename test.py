eedd3a8f40dba21ffc993d84d06c90401ac5f1bd
heleiduan <duanhelei@gmail.com>
2023-01-20 13:19:52 -0800

    make tests smoother

--- test.py
+++ test.py
@@ -4,11 +4,12 @@ from testing.test_ar_sim import (
     test_ar_connect,
     test_ar_api_goto,
     test_ar_sim_forward,
-    test_ar_sim_llapi
+    test_ar_sim_llapi_walking_handover,
+    test_ar_sim_llapi_teststand
 )
 
-for _ in range(3):
-    # asyncio.run(test_ar_connect())
-    # asyncio.run(test_ar_api_goto())
-    # asyncio.run(test_ar_sim_forward())
-    asyncio.run(test_ar_sim_llapi())
+asyncio.run(test_ar_connect())
+asyncio.run(test_ar_api_goto())
+asyncio.run(test_ar_sim_forward())
+asyncio.run(test_ar_sim_llapi_teststand())
+asyncio.run(test_ar_sim_llapi_walking_handover())
--- testing/test_ar_sim.py
+++ testing/test_ar_sim.py
@@ -1,117 +1,20 @@
-from sim.digit_sim import DigitArSim
 import asyncio
 import agility.messages as msg
 import numpy as np
 
-from sim.digit_sim.digit_ar_sim.llapi.llapictypes import (
-    llapi_connected,
+from sim.digit_sim import DigitArSim
+from .common import (
+    SIM_PATH, 
+    ROBOT_CONFIG,
+    MOTOR_POSITION_SET
 )
 
 """
-The following is more like a test file that test the ar-control above.
-We can also fit below inside arcontrol class or into GenericSim or exact env
+The followings are independent tests for wrappers around Agility's ar-control, pysdk, and LLAPI.
 """
 
-sim_path = './sim/digit_sim/digit_ar_sim/ar-control'
-conf = []
-conf += ["""
-# Add robot to world
-[[model-list]]
-model = "robot"
-pose = {xyz = [0, 0, 1.0]}
-
-[simulator]
-free-run=true
-
-[planning]
-initial-operation-mode = "locomotion"
-
-[lowlevelapi]
-enable = true
-# Time in seconds between Low-level API packets before control program
-# disables robot
-timeout = 0.05
-# Port for incoming and outgoing Low-level API communications.
-# Change if these settings interfere with any other programs or network
-# Traffic on the payload computer. Must match settings in lowlevelapi.c
-# (lines 181 and 183). These should not be changed under normal operation
-listen-port = 25500
-send-port = 25501
-"""]
-
-
-motor_pos_set={}
-motor_pos_set['pos1'] = [
--0.0462933,
--0.0265814,
-0.19299,
--0.3,
--0.0235182,
--0.0571617,
-0.0462933,
-0.0265814,
--0.19299,
-0.3,
--0.0235182,
-0.0571617,
--0.3,
-0.943845,
-0.0,
-0.3633,
-0.3,
--0.943845,
-0.0,
--0.3633,
-]
-
-motor_pos_set['pos2'] = [
-0.332,
--0.0265814,
-0.19299,
-0.218647,
-0.0235182,
--0.0571617,
--0.332,
-0.0265814,
--0.19299,
--0.218647,
--0.0235182,
-0.0571617,
--0.106437,
-0.89488,
--0.00867663,
-0.344684,
-0.106339,
--0.894918,
-0.00889888,
--0.344627
-]
-
-motor_pos_set['pos3'] = [
--0.0462933,
--0.5265814,
-0.19299,
--0.3,
--0.0235182,
--0.0571617,
-0.0462933,
-0.5265814,
--0.19299,
-0.3,
--0.0235182,
-0.0571617,
--0.3,
-0.0943845,
-0.0,
--0.3633,
-0.3,
--0.0943845,
-0.0,
-0.3633,
-]
-
 async def test_ar_connect():
-    ar = DigitArSim(path_to_ar_control=sim_path, args=conf)
+    ar = DigitArSim(path_to_ar_control=SIM_PATH, args=ROBOT_CONFIG)
     # Need to call this to connect API with simulator
     await ar.setup()
     await ar.reset()
@@ -121,7 +24,7 @@ async def test_ar_connect():
     print("Closed all")
 
 async def test_ar_api_goto():
-    ar = DigitArSim(path_to_ar_control=sim_path, args=conf)
+    ar = DigitArSim(path_to_ar_control=SIM_PATH, args=ROBOT_CONFIG)
     # Need to call this to connect API with simulator
     await ar.setup()
     await ar.reset()
@@ -132,7 +35,7 @@ async def test_ar_api_goto():
     print("Closed all")
     
 async def test_ar_sim_forward():
-    ar = DigitArSim(path_to_ar_control=sim_path, args=conf)
+    ar = DigitArSim(path_to_ar_control=SIM_PATH, args=ROBOT_CONFIG)
     # Need to call this to connect API with simulator
     await ar.setup()
     await ar.reset()
@@ -144,14 +47,14 @@ async def test_ar_sim_forward():
         await ar.sim_forward(dt=1)
         # await ar.api.send(msg.SimulatorPause())
         # await asyncio.sleep(0.1)
-        print("time={:3.2f}, x={:3.1f}".format(ar.t, ar.obs.base.translation[0]))
-    if np.abs(ar.obs.base.translation[0] - 1) < 1e-2:
+        print("time={:3.2f}, x={:3.1f}".format(ar.t, ar.get_base_translation()[0]))
+    if np.abs(ar.get_base_translation()[0] - 1) < 1e-2:
         print("success")
     await ar.close_all()
     print("Closed all")
     
-async def test_ar_sim_llapi():
-    ar = DigitArSim(path_to_ar_control=sim_path, args=conf)
+async def test_ar_sim_llapi_walking_handover():
+    ar = DigitArSim(path_to_ar_control=SIM_PATH, args=ROBOT_CONFIG)
     # Need to call this to connect API with simulator
     await ar.setup()
     await ar.reset()
@@ -169,7 +72,7 @@ async def test_ar_sim_llapi():
     # 1. Write/Send commands struct
     # 2. Toggle apply_command to be True
     # 3. Enable LLAPI mode via API call
-    await ar.sim_forward(dt=1, actions=motor_pos_set['pos1'])
+    await ar.sim_forward(dt=1, actions=MOTOR_POSITION_SET['pos1'])
     act = (await ar.api.query(msg.GetActionCommand()))
     if act['action'] != 'action-set-operation-mode':
         await ar.api.send(msg.ActionSetOperationMode(mode="low-level-api"))
@@ -180,11 +83,11 @@ async def test_ar_sim_llapi():
     # NOTE: still mystery about when Damping mode will turn on
     for i in range(10):
         if i%3==1:
-            await ar.sim_forward(dt=0.5, actions=motor_pos_set['pos1'])
+            await ar.sim_forward(dt=0.5, actions=MOTOR_POSITION_SET['pos1'])
         elif i%3==2:
-            await ar.sim_forward(dt=0.5, actions=motor_pos_set['pos2'])
+            await ar.sim_forward(dt=0.5, actions=MOTOR_POSITION_SET['pos2'])
         else:
-            await ar.sim_forward(dt=0.5, actions=motor_pos_set['pos3'])
+            await ar.sim_forward(dt=0.5, actions=MOTOR_POSITION_SET['pos3'])
         print("time={: 4.2f}, z-vel={: 3.2f}, llapi-connect={}".format(
             ar.t, ar.get_base_linear_velocity()[2], ar.is_llapi_connected()))
 
@@ -195,5 +98,66 @@ async def test_ar_sim_llapi():
         #     break
         # await asyncio.sleep(5)
 
+    await ar.close_all()
+    print("Closed all")
+
+async def test_ar_sim_llapi_teststand():
+    conf = ["""
+# Add robot to world
+[[model-list]]
+model = "robot"
+pose = {xyz = [0, 0, 1.0]}
+fixed=true
+
+[simulator]
+free-run=true
+[simulator.robot]
+initial-configuration = "test-stand"
+
+
+[planning]
+initial-operation-mode = "disabled"
+
+[lowlevelapi]
+enable = true
+# Time in seconds between Low-level API packets before control program
+# disables robot
+timeout = 0.05
+# Port for incoming and outgoing Low-level API communications.
+# Change if these settings interfere with any other programs or network
+# Traffic on the payload computer. Must match settings in lowlevelapi.c
+# (lines 181 and 183). These should not be changed under normal operation
+listen-port = 25500
+send-port = 25501
+"""]
+    ar = DigitArSim(path_to_ar_control=SIM_PATH, args=conf)
+    # Need to call this to connect API with simulator
+    await ar.setup()
+    await ar.reset()
+    
+    # Test LLAPI mode toggle
+    # To fully enable LLAPI and take over ar-control. Call sim_forward with actions for step 1 & 2
+    # and API call for step 3.
+    # 1. Write/Send commands struct
+    # 2. Toggle apply_command to be True
+    # 3. Enable LLAPI mode via API call
+    await ar.sim_forward(dt=1, actions=MOTOR_POSITION_SET['pos1'])
+    act = (await ar.api.query(msg.GetActionCommand()))
+    if act['action'] != 'action-set-operation-mode':
+        await ar.api.send(msg.ActionSetOperationMode(mode="low-level-api"))
+    print("LLAPI ON")
+
+    # Since LLAPI mode on, we can keep sending to execute custom commands.
+    # Robot will fall in this test case. 
+    # NOTE: still mystery about when Damping mode will turn on
+    for i in range(30):
+        if i%3==1:
+            await ar.sim_forward(dt=0.5, actions=MOTOR_POSITION_SET['pos1'])
+        elif i%3==2:
+            await ar.sim_forward(dt=0.5, actions=MOTOR_POSITION_SET['pos2'])
+        else:
+            await ar.sim_forward(dt=0.5, actions=MOTOR_POSITION_SET['pos3'])
+        print("time={: 4.2f}, z-vel={: 3.2f}, llapi-connect={}".format(
+            ar.t, ar.get_base_linear_velocity()[2], ar.is_llapi_connected()))
     await ar.close_all()
     print("Closed all")
