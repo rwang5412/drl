1c1dd343199922bc94e0142c992935030ec6d27c
heleiduan <duanhelei@gmail.com>
2023-02-06 18:56:50 -0800

    typo

--- env/__init__.py
+++ env/__init__.py
@@ -3,7 +3,7 @@ from .cassie.cassieenv import CassieEnv
 from .digit.digitenv import DigitEnv
 
 # Cassie related envs
-from .cassie.cassieenvclock.cassieenvclock import CassieEnvClock
+from .cassie.cassieenv_clock.cassieenvclock import CassieEnvClock
 
 # Digit related envs
 from .digit.digitenv_clock.digitenvclock import DigitEnvClock
