b25e86f0cf23794d8c59af0f703a8df89a800e99
heleiduan <duanhelei@gmail.com>
2023-02-09 20:13:07 -0800

    clean up

--- eval.py
+++ eval.py
@@ -1,12 +1,20 @@
 import torch
 import argparse
+import sys
 
-from util.eval_utils import simple_eval
+from util.evaluation_factory import simple_eval
 
 if __name__ == "__main__":
 
+    # Choose which evaluation type
+    try:
+        evaluation_type = sys.argv[1]
+        sys.argv = sys.argv[1:]
+    except:
+        raise RuntimeError("Need to choose evaluation type from ['simple','ui']. Or add a new evaluation type.")
+
     """
-    These parsers will be removed and replaced by loading args from dict.
+    These parsers Env + NN will be removed and replaced by loading args from dict.
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('--env-name', default="CassieEnvClock")
@@ -17,9 +25,12 @@ if __name__ == "__main__":
     parser.add_argument('--dynamics-randomization', default=False)
     parser.add_argument('--terrain', default=False)
     args = parser.parse_args()
-
     from nn.actor import LSTMActor, FFActor
     actor = LSTMActor(input_dim=42, action_dim=10, layers=[128,128], bounded=False, learn_std=False, std=0.1)
     actor_state_dict = torch.load('./pretrained_models/speed_locomotion_vonmises_clock.pt', map_location='cpu')
     actor.load_state_dict(actor_state_dict)
-    simple_eval(actor=actor, args=args)
+
+    if evaluation_type == 'simple':
+        simple_eval(actor=actor, args=args)
+    else:
+        raise RuntimeError(f"This evaluation type {evaluation_type} has not been implemented.")
