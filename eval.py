279d84b4057cfc8b2a225348143aeff144281238
heleiduan <duanhelei@gmail.com>
2023-02-09 19:35:46 -0800

    cleanup

--- eval.py
+++ eval.py
@@ -6,17 +6,19 @@ import numpy as np
 
 from util.env_factory import env_factory
 
-def simple_eval(actor, run_args, traj_len_max=1000):
-    env = env_factory(**vars(run_args))()
-    
+def simple_eval(actor, args, episode_length_max=300):
+    # Load environment
+    env = env_factory(**vars(args))()
+
     with torch.no_grad():
         state = env.reset()
         done = False
-        traj_len = 0
-        
+        episode_length = 0
+        episode_reward = []
+
         if hasattr(actor, 'init_hidden_state'):
             actor.init_hidden_state()
-        
+
         env.sim.viewer_init()
         render_state = env.sim.viewer_render()
         while render_state:
@@ -25,18 +27,24 @@ def simple_eval(actor, run_args, traj_len_max=1000):
                 state = torch.Tensor(state).float()
                 action = actor(state).numpy()
                 state, reward, done, _ = env.step(action)
-                traj_len += 1
+                episode_length += 1
+                episode_reward.append(reward)
             render_state = env.sim.viewer_render()
-            delaytime = max(0, 50/2000 - (time.time() - start_time))
+            delaytime = max(0, env.default_policy_rate/2000 - (time.time() - start_time))
             time.sleep(delaytime)
-            if traj_len == traj_len_max or done:
+            if episode_length == episode_length_max or done:
+                print(f"Episode length = {episode_length}, Average reward is {np.mean(episode_reward)}.")
                 state = env.reset()
-                traj_len = 0
+                episode_length = 0
+                if hasattr(actor, 'init_hidden_state'):
+                    actor.init_hidden_state()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    """Environment specific kwargs
+
     """
+    These parsers will be removed and replaced by loading args from dict.
+    """
+    parser = argparse.ArgumentParser()
     parser.add_argument('--env-name', default="CassieEnvClock")
     parser.add_argument('--simulator-type', default="mujoco")
     parser.add_argument('--clock-type', default="von_mises")
@@ -44,21 +52,12 @@ if __name__ == "__main__":
     parser.add_argument('--policy-rate', default=40)
     parser.add_argument('--dynamics-randomization', default=False)
     parser.add_argument('--terrain', default=False)
-    
-    """Actor/Critic specific kwargs
-    """
-    # parser.add_argument('--input-dim', type=int, default=1)
-    # parser.add_argument('--action-dim', type=int, default=1)
-    # parser.add_argument('--layers', nargs='+', type=int, default=[128,128])
-    # parser.add_argument('--learn-std', default=False)
-    # parser.add_argument('--std', default=1)
-    
+
     # Process args
     args = parser.parse_args()
-    # print(args)
-    
+
     from nn.actor import LSTMActor, FFActor
     actor = LSTMActor(input_dim=42, action_dim=10, layers=[128,128], bounded=False, learn_std=False, std=0.1)
     actor_state_dict = torch.load('./pretrained_models/speed_locomotion_vonmises_clock.pt', map_location='cpu')
     actor.load_state_dict(actor_state_dict)
-    simple_eval(actor=actor, run_args=args)
+    simple_eval(actor=actor, args=args)
