9c2d48935ea51c5613abbd056380f79fa46ddc2c
heleiduan <duanhelei@gmail.com>
2023-02-13 12:17:41 -0800

    rendering is actually very bad

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
 import argparse
-import asyncio
-import os
-import pickle
-import sys
-
-import testing.test_sim as test_sim
-import testing.test_env as test_env
-
-from testing.test_ar_sim import (
-    test_ar_connect,
-    test_ar_api_goto,
-    test_ar_sim_forward,
-    test_ar_sim_llapi_walking_handover,
-    test_ar_sim_llapi_teststand
-)
-from testing.test_clock import test_all_clocks
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,14 +13,16 @@ if __name__ == "__main__":
     if args.algo:
         pass
     if args.env:
+        import testing.test_env as test_env
         test_env.test_all_env()
     if args.sim:
-        import testing.test_sim as ts
-        ts.test_all_sim()
+        import testing.test_sim as test_sim
+        test_sim.test_all_sim()
     if args.render:
         from testing.test_offscreen_render import test_offscreen_rendering
         test_offscreen_rendering()
     if args.ar:        
+        import asyncio
         from testing.test_ar_sim import (
             test_ar_connect,
             test_ar_api_goto,
@@ -50,6 +36,7 @@ if __name__ == "__main__":
         asyncio.run(test_ar_sim_llapi_teststand())
         asyncio.run(test_ar_sim_llapi_walking_handover())
     if args.clock:
+        from testing.test_clock import test_all_clocks
         test_all_clocks()
     if args.nn:
         from testing.test_nn import test_nn
--- testing/test_offscreen_render.py
+++ testing/test_offscreen_render.py
@@ -4,7 +4,7 @@
 def test_offscreen_rendering():
 	# Need to update env variable before import mujoco
 	import os
-	gl_option = 'glx'
+	gl_option = 'egl'
 	os.environ['MUJOCO_GL']=gl_option
 
 	import time
@@ -12,60 +12,67 @@ def test_offscreen_rendering():
 	import matplotlib.pyplot as plt
 	import mediapy
 	import mujoco
-	from sim import MjCassieSim, DigitMjSim
+	from sim import MjCassieSim
 
 	# Check if the env variable is correct
 	if "MUJOCO_GL" in os.environ:
 		assert os.getenv('MUJOCO_GL') == gl_option, f"GL option is {os.getenv('MUJOCO_GL')} but want to load {gl_option}."
 		print(os.getenv('PYOPENGL_PLATFORM'))
 
-	# size = [25, 50, 100, 150, 200, 250, 300, 350, 400, 480]
-	size = [400]
-	avg_list = []
+	size = [25, 50, 100, 150, 200, 250, 300, 350, 400, 480]
+	# size = [400]
+	sim_time_avg_list = []
+	render_time_avg_list = []
+	time_render_ratio = []
 	for s in size:
+		# Init mujoc sim and optional viewer to verify
 		sim = MjCassieSim()
 		sim.reset()
-		# renderer = mujoco.Renderer(sim.model, height=s, width=s)
-		# print("Verify the Gl context object, ", renderer._gl_context)
-		sim.viewer_init()
-		render_state = sim.viewer_render()
-		# renderer.enable_depth_rendering()
+		camera_name = "forward-chest-realsense-d435/depth/image-rect"
+		# sim.viewer_init(camera_id=camera_name, height=500, width=500)
+		# render_state = sim.viewer_render()
 
-		time_list = []
+		# Init renderer that reads the same model/data
+		renderer = mujoco.Renderer(sim.model, height=s, width=s)
+		print("Verify the Gl context object, ", renderer._gl_context)
+		renderer.enable_depth_rendering()
+
+		time_raw_sim_list = []
+		time_render_depth = []
 		frames = []
-		camera_name = "egocentric"
-		# for _ in range(50):
-		# 	for _ in range(50):
-		# 		sim.sim_forward()
-		# 	start = time.monotonic()
-		# 	renderer.update_scene(sim.data, camera=camera_name)
-		# 	img = renderer.render().copy()
-		# 	if renderer._depth_rendering:
-		# 		img -= img.min()
-		# 		img /= 2 * img[img <= 1].mean()
-		# 		img = 255 * np.clip(img, 0, 1)
-		# 	time_list.append(time.monotonic() - start)
-		# 	frames.append(img.astype(np.uint8))
-		# 	if sim.get_base_position()[2] < 0.5:
-		# 		break
-		# mean_time = np.mean(np.array(time_list))
-		# # print("mean time to update scene and render ", mean_time)
-		# avg_list.append(mean_time)
-
-		while render_state:
+		while True:
+			# if not sim.viewer_paused():
+			for _ in range(50):
+				start = time.time()
+				sim.sim_forward()
+			time_raw_sim_list.append(time.time() - start)
+			# render_state = sim.viewer_render()
+			# Render offscreen
 			start_t = time.time()
-			if not sim.viewer_paused():
-				for _ in range(50):
-					start = time.time()
-					sim.sim_forward()
-					time_list.append(time.time() - start)
-			render_state = sim.viewer_render()
-			mean_time = np.mean(np.array(time_list))
-			print("sim rate ", 1/mean_time)
-	# mediapy.write_video("test.mp4", frames, fps=6)
-	# plt.imshow(img.astype(np.uint8), cmap='gray')
-	# plt.colorbar(label='Distance to Camera')
-	# plt.show()
-	# plt.plot(size, avg_list)
-	# plt.show()
+			renderer.update_scene(sim.data, camera=camera_name)
+			img = renderer.render()
+			time_render_depth.append(time.time() - start_t)
+			frames.append(img)
+
+			if sim.get_base_position()[2] < 0.5:
+				mean_time_sim = np.mean(np.array(time_raw_sim_list))
+				mean_time_render = np.mean(np.array(time_render_depth))
+				sim_time_avg_list.append(mean_time_sim)
+				render_time_avg_list.append(mean_time_render)
+				time_render_ratio.append(100*mean_time_render / (mean_time_sim+mean_time_render))
+				break
+	mediapy.write_video("test.mp4", frames, fps=30)
+	fig, ((ax1, ax3), (ax2, ax4)) = plt.subplots(2, 2)
+	ax1.plot(size, sim_time_avg_list)
+	ax1.set_title('sim time [s] per policy step')
+	ax2.plot(size, render_time_avg_list)
+	ax2.set_title('render time [s] per policy step')
+	ax3.plot(size, time_render_ratio)
+	ax3.set_title('render ratio [%] per policy step')
+	wall_time = [t * 300e6 / 3600 for t in render_time_avg_list]
+	ax4.plot(size, wall_time)
+	ax4.set_title('additional walk clock time [hours]\nfor 300M samples')
+	ax4.set_xlabel('image size')
+	fig.tight_layout()
+	plt.show()
 	print("Passed offscreen rendering test!")
