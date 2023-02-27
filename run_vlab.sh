7c85c2ba2abf73e5772eac9391fb5af0a7854fe7
heleiduan <duanhelei@gmail.com>
2023-02-27 15:25:02 -0800

    fix command

--- run_vlab.sh
+++ run_vlab.sh
@@ -6,10 +6,8 @@ source /export/software/anaconda3/bin/activate
 conda activate mjenv
 
 python train.py ppo \
---env-name 'CassieEnvClock' --simulator_type 'mujoco' \
---policy_rate 50 \
---reward_name 'locomotion_linear_clock_reward' --clock_type 'linear' \
---run_name 'init_test' \
+--env-name 'CassieStone' \
+--run_name 'init_test_stone' \
 --layers 64,64 --discount 0.95 \
 --std 0.13 --mirror 1 --batch_size 32 --num_steps 50000 --a_lr 0.0003 --c_lr 0.0003 --epochs 5 \
---traj_len 300 --timesteps 4000000000 --workers 56 --arch ff --wandb
+--traj_len 300 --timesteps 4000000000 --workers 56 --arch lstm --wandb
