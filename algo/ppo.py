00cc9f83cb1734eaf21e4946abc4805c3e84b486
Jeremy Dao <jeremydao913@gmail.com>
2023-02-27 17:03:05 -0800

    remove extra env_name arg

--- algo/ppo.py
+++ algo/ppo.py
@@ -474,7 +474,6 @@ def run_experiment(parser, env_name):
 
     # Create callable env_fn for parallelized envs
     env_fn = env_factory(env_name, env_args)
-    args.env_name = env_name
 
     # Create nn modules. Append more in here and add_nn_parser() if needed
     nn_args.obs_dim = env_fn().observation_size
