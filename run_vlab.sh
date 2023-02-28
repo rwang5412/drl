1ade5e84e55efeeb6559110b6ddef719f3e64c7f
heleiduan <duanhelei@126.com>
2023-02-27 16:27:44 -0800

    sync vlab

--- run_vlab.sh
+++ run_vlab.sh
@@ -6,10 +6,8 @@ source /export/software/anaconda3/bin/activate
 conda activate mjenv
 
 python train.py ppo \
---env-name 'CassieEnvClock' --simulator-type 'mujoco' \
---policy-rate 50 \
---reward-name 'locomotion_linear_clock_reward' --clock-type 'linear' \
---run-name 'fix_in_linear_randratio_lstm' \
+--env-name 'CassieStone' \
+--run-name 'stone_flat' \
 --layers 64,64 --discount 0.95 \
 --std 0.13 --mirror 1 --batch-size 32 --num-steps 50000 --a-lr 0.0003 --c-lr 0.0003 --epochs 5 \
 --traj-len 300 --timesteps 4000000000 --workers 56 --arch lstm --wandb
