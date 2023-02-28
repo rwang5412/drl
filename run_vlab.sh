b210e716df43d6c8496b05d24c46686d1ae53579
heleiduan <duanhelei@gmail.com>
2023-02-27 16:17:07 -0800

    update bash with hyphen

--- run_vlab.sh
+++ run_vlab.sh
@@ -6,10 +6,10 @@ source /export/software/anaconda3/bin/activate
 conda activate mjenv
 
 python train.py ppo \
---env-name 'CassieEnvClock' --simulator_type 'mujoco' \
---policy_rate 50 \
---reward_name 'locomotion_linear_clock_reward' --clock_type 'linear' \
---run_name 'init_test' \
+--env-name 'CassieEnvClock' --simulator-type 'mujoco' \
+--policy-rate 50 \
+--reward-name 'locomotion_linear_clock_reward' --clock-type 'linear' \
+--run-name 'fix_in_linear_randratio_lstm' \
 --layers 64,64 --discount 0.95 \
---std 0.13 --mirror 1 --batch_size 32 --num_steps 50000 --a_lr 0.0003 --c_lr 0.0003 --epochs 5 \
---traj_len 300 --timesteps 4000000000 --workers 56 --arch ff --wandb
+--std 0.13 --mirror 1 --batch-size 32 --num-steps 50000 --a-lr 0.0003 --c-lr 0.0003 --epochs 5 \
+--traj-len 300 --timesteps 4000000000 --workers 56 --arch lstm --wandb
