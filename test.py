bbd4c821532b82bfd7734cab03d81766ac0d41f2
yu-fz <fngzhou.yu@gmail.com>
2023-03-08 14:03:31 -0800

    rebase test.py with develop

--- test.py
+++ test.py
@@ -1,22 +1,4 @@
 import argparse
-import asyncio
-import os
-import pickle
-import sys
-
-import testing.test_sim as test_sim
-import testing.test_env as test_env
-import testing.test_drake_integration as test_drake
-
-from testing.test_ar_sim import (
-    test_ar_connect,
-    test_ar_api_goto,
-    test_ar_sim_forward,
-    test_ar_sim_llapi_walking_handover,
-    test_ar_sim_llapi_teststand
-)
-from testing.test_algo import test_all_algos
-from testing.test_clock import test_all_clocks
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,8 +11,8 @@ if __name__ == "__main__":
     parser.add_argument("--nn", default=False, action='store_true')
     parser.add_argument("--mirror", default=False, action='store_true')
     parser.add_argument("--render", default=False, action='store_true')
-    parser.add_argument("--all", default=False, action='store_true')
     parser.add_argument("--drake", default=False, action='store_true')
+    parser.add_argument("--all", default=False, action='store_true')
     args = parser.parse_args()
 
     if args.all:
@@ -39,7 +21,7 @@ if __name__ == "__main__":
         args.sim = True
         args.clock = True
         args.nn = True
-        args.drake = True
+        args.drake = True 
 
     if args.algo:
         from testing.test_algo import test_all_algos
@@ -77,4 +59,5 @@ if __name__ == "__main__":
         from testing.test_mirror import test_mirror
         test_mirror()
     if args.drake:
-        test_drake.test_all()
+        from testing.test_drake_integration import test_all
+        test_all()
