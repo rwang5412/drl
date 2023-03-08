8de3537868bd6ca70319aad5ba609c5d3b375683
yu-fz <fngzhou.yu@gmail.com>
2023-03-08 13:23:07 -0800

    updated duality package version, added tests, update readme with submodule directions

--- testing/test_drake_integration.py
+++ testing/test_drake_integration.py
@@ -2,14 +2,32 @@ import argparse
 import sys
 import numpy as np
 
-from drail_drake.digit.digit_arm_diff_ik import DigitArmDiffInverseKinematics
+from duality.digit.digit_arm_diff_ik import DigitArmDiffInverseKinematics
 from util.colors import OKGREEN, FAIL, ENDC
 
-def test_drail_drake_imports():
+def test_duality_imports():
     try: 
-        from drail_drake.digit import DigitArmDiffInverseKinematics
-        from drail_drake.digit import DigitDrakeSim
+        from duality.digit import DigitArmDiffInverseKinematics
+        from duality.digit import DigitDrakeSim
+        digit = DigitArmDiffInverseKinematics()
+        digit.wire_arm_V_G_tracking_diff_ik()
     except ImportError:
         print(f"{FAIL}")
-    
-    print(f"{OKGREEN}Passed all drail drake package import tests \u2713{ENDC}")
+
+    print(f"{OKGREEN}Passed all duality package import tests \u2713{ENDC}")
+
+def test_duality_diff_ik_constructor():
+    try: 
+        from duality.digit import DigitArmDiffInverseKinematics
+        digit = DigitArmDiffInverseKinematics()
+        digit.wire_arm_V_G_tracking_diff_ik()
+        assert(digit.plant is not None)
+        assert(digit.plant_context is not None)
+    except ImportError:
+        print(f"{FAIL}")
+
+    print(f"{OKGREEN}Passed duality diff ik function import tests \u2713{ENDC}")
+
+def test_all():
+    test_duality_imports()
+    test_duality_diff_ik_constructor()
