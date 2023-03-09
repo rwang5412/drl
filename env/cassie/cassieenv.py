tracker weighting fix

env/cassie/cassieenv.py
@@ -50,8 +50,8 @@ class CassieEnv(GenericEnv):
         self.orient_add = 0
         # self.trackers = [self.update_tracker_grf,
                         #  self.update_tracker_velocity]
-        self.trackers = {self.update_tracker_grf: {"frequency": 50},
-                         self.update_tracker_velocity: {"frequency": 50}
+        self.trackers = {self.update_tracker_grf: {"frequency": 500},
+                         self.update_tracker_velocity: {"frequency": 100}
                         }
         # Double check tracker frequencies and convert to number of sim steps
         for tracker, tracker_dict in self.trackers.items():
@@ -108,6 +108,10 @@ class CassieEnv(GenericEnv):
         Args:
             action (np.ndarray): Actions from policy inference.
         """
+        count = 0
+        # Reset trackers
+        for tracker_fn, tracker_dict in self.trackers.items():
+            tracker_fn(weighting = 0, sim_step = 0)
         for sim_step in range(simulator_repeat_steps):
             # Explore around neutral offset
             setpoint = action + self.offset
@@ -118,11 +122,10 @@ class CassieEnv(GenericEnv):
             self.sim.sim_forward()
             # Update simulation trackers (signals higher than policy rate, like GRF, etc)
             for tracker_fn, tracker_dict in self.trackers.items():
-                if sim_step % tracker_dict["num_step"] == 0:
-                    tracker_fn(weighting = 1 / tracker_dict["num_step"], sim_step = sim_step)
-
-            # for tracker in self.trackers:
-            #     tracker(weighting=1/simulator_repeat_steps, sim_step=sim_step)
+                if (sim_step + 1) % tracker_dict["num_step"] == 0:
+                    count += 1
+                    tracker_fn(weighting = 1 / (simulator_repeat_steps / tracker_dict["num_step"]),
+                               sim_step = sim_step)
 
     def get_robot_state(self):
         """Get standard robot prorioceptive states. Sub-env can override this function to define its